eef3a9bb4e2a718e93885d2df22e028dc7f1eec0
Denise Lee <denise.lee.90@gmail.com>
2020-08-16 07:24:37 +0000

    added category and tagsrelationship for products

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from .models import Product
+from .models import Product, Tags, Category
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('name', 'description', 'price')
+        fields = ('name', 'description', 'price', 'category', 'tags')
--- products/models.py
+++ products/models.py
@@ -4,11 +4,25 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Tags(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return self.title
+
 class Product(models.Model):
     name = models.CharField(blank=False, max_length=255, validators=[
                              MinLengthValidator(3)])
     description = models.TextField(blank=False)
     price = models.IntegerField(blank=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tags)
 
     def __str__(self):
         return self.name
